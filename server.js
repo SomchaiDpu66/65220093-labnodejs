fb8a072e310fa9c6507aab508c80683c2924b748
DESKTOP-BM5R6ES\DELL-LAB 6110X <640117010052@dpu.ac.th>
2023-02-26 14:54:57 +0700

    New Change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3100;
+const port = process.env.PORT || 9000;
 const cors = require("cors");
 
 const fullname = "65230093 ,Somchai Karapitak";
@@ -8,83 +8,43 @@ const fullname = "65230093 ,Somchai Karapitak";
 const products = [
   {
     id: "1",
-    name: "ผ้าพันคอLiverpool เรดเครสต์",
+    name: "Apple Watch series 8",
     category: "Apple",
     price: 15000,
     cover:
-      "https://store.liverpoolfc.com/media/catalog/product/cache/dd504f005c0b90ffe1b9fb2344db1a87/A/1/A10716.jpg",
+      "https://www.flashfly.net/wp/wp-content/uploads/2022/05/FTX1B5hWIAA6GRr-2-800x600.jpeg",
   },
   {
     id: "2",
-    name: "LFC Crest Badge Set",
+    name: "Iphone 14 PRO",
     category: "Apple",
     price: 35000,
     cover:
-      "https://store.liverpoolfc.com/media/catalog/product/cache/75834f51694404f8a86d1a825ae7de7e/a/2/a21bd03_4_1.jpg",
+      "https://promotions.co.th/wp-content/uploads/2022/09/iphone14-pro-max.jpg",
   },
   {
     id: "3",
-    name: "GAMBOL Liverpool FC SUPER SUP (Size 36-46)",
+    name: "จอมอนิเตอร์ 27นิ้ว จอคอม 75HZ หน้าจอโค้ง จอเกมมิ่ง LED Gaming monitor จอมอนิเตอร์เกมมิ่ง VGA HDMI จอมอนิเตอ สปอตสินค้า",
     category: "LG",
     price: 5600,
     cover:
-      "https://f.btwcdn.com/store-42285/product/711901f1-e710-d2b0-5e99-6241869c16a2.jpg",
+      "https://www.techmoblog.com/uploads/content_images/201610/img_1476165152_81877887513b.jpg",
   },
   {
     id: "4",
-    name: "HEADSET (หูฟัง) ONILiverpool F.C. Crest Gaming Chair",
+    name: "HEADSET (หูฟัง) ONIKUMA K9 - 3.5 MM (PINK)",
     category: "ONIKUMA ",
     price: 990,
     cover:
-      "https://cdn.shopify.com/s/files/1/0579/6085/7805/products/FanSeats_LiverPool_Basic_Gaming_Chair.jpg?v=1650974330",
+      "https://www.jib.co.th/img_master/product/original/20210803092821_41661_66_1.jpg",
   },
   {
     id: "5",
-    name: "Liverpool F.C. Small Crest Stool",
+    name: "Apple iPad Gen 9 | iStudio by copperwired",
     category: "Apple",
     price: 12900,
     cover:
-      "https://cdn.shopify.com/s/files/1/0579/6085/7805/products/LCFC-Kids-Crest-Stool-Front.png?v=1626771340",
-  },
-  {
-    id: "6",
-    name: "Nike Football Liverpool FC Crest Logo Cap In Red",
-    category: "Apple",
-    price: 12900,
-    cover:
-      "https://image.goxip.com/Fwnovbw52iGm2qDnqWaw6DK50-Q=/fit-in/500x500/filters:format(jpg):quality(80):fill(white)/https:%2F%2Fimages.asos-media.com%2Fproducts%2Fnike-football-liverpool-fc-crest-logo-cap-in-red%2F23557865-1-red%3F$XXL$",
-  },
-  {
-    id: "7",
-    name: "Bagpack Liverpool",
-    category: "Apple",
-    price: 12900,
-    cover:
-      "https://n4.sdlcdn.com/imgs/a/p/q/Mm-Products-Liverpool-Bagpack-SDL598743038-1-f1889.jpg",
-  },
-  {
-    id: "8",
-    name: " The Best Selection of Liverpool FC and Nike Merchandise",
-    category: "Apple",
-    price: 12900,
-    cover:
-      "https://cdn.shopify.com/s/files/1/0462/0120/8982/products/DM1843-609-PHSFH001_400x.jpg?v=1676519255",
-  },
-  {
-    id: "9",
-    name: " Liverpool Keychain ",
-    category: "Apple",
-    price: 12900,
-    cover:
-      "https://theproductlab.in/wp-content/uploads/2021/12/Liverpool-keychains.jpg",
-  },
-  {
-    id: "10",
-    name: " Liverpool FC HERO (Size 36-44)",
-    category: "Apple",
-    price: 12900,
-    cover:
-      "https://f.btwcdn.com/store-42285/product/daca90cf-2921-fab2-d666-625fdb1521d8.jpg",
+      "https://store.ais.co.th/media/catalog/product/i/p/ipad_10.2_inch_wi-fi_space_gray_pdp_image_position-1b__th_2.jpg",
   },
 ];
 
@@ -106,5 +66,5 @@ app.get("/products/:id", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("Application is running on port 3100");
+  console.log("Application is running on port 9000");
 });
